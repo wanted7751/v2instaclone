bfdcf35d6f85f64c232f5167d343ec48613c2bf1
parkmimho <smj0441@daum.net>
2019-01-31 20:40:51 +0900

    #1-25 Customizing the Django Admin

--- gram/images/models.py
+++ gram/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from gram.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,29 +11,38 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
 
     """ Image Model """
 
     file = models.ImageField()
     location = models.CharField(max_length=140)
-    caption = models.TextField
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    caption = models.TextField(null=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return '{}-{}'.format(self.location, self.caption)
 
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
 
     """ Comment Model """
 
     message = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image =  models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image =  models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.message, self.creator)
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
 
     """ Like Model """
 
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image,  on_delete=models.PROTECT, null=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image,  on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format( self.creator.username, self.image)
--- gram/users/models.py
+++ gram/users/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 
-
+@python_2_unicode_compatible
 class User(AbstractUser):
 
     """"User Model"""
