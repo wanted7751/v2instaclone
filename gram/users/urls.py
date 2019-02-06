dc78dbac9dc515301d2d1f7cfd54fe455ac25330
parkmimho <smj0441@daum.net>
2019-02-06 10:53:32 +0900

    #1-57 Search users by username

--- gram/users/urls.py
+++ gram/users/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("explore/", view=views.ExploreUsers.as_view(), name="explore_users"),
     path("<int:user_id>/follow/", view=views.FollowUsers.as_view(), name="follow_users"),
     path("<int:user_id>/unfollow/", view=views.UnFollowUsers.as_view(), name="follow_users"),
+    path("search/", view=views.Search.as_view(), name="search"),
+    path("<str:username>/followers/", view=views.UserFollowers.as_view(), name="user_followers"),
+    path("<str:username>/following/", view=views.UserFollowing.as_view(), name="user_following"),
     path("<str:username>/", view=views.UserProfile.as_view(), name="user_profile"),
-    path("<str:username>/followers", view=views.UserFollowers.as_view(), name="user_followers"),
-    path("<str:username>/following", view=views.UserFollowing.as_view(), name="user_following")
+    
 ]
