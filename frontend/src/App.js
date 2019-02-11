ad21cb4de585d04d34eac76e69e719b2cd5ac68c
parkmimho <smj0441@daum.net>
2019-02-11 10:54:30 +0900

    2-9 Adding CSS Modules to Webpack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-
+import logo from './logo.svg';
+import styles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-     <div>good</div>
+      <div className={styles.App}>
+        <header className={styles.App__header}>
+          <img src={logo} className={styles.App__logo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={styles.App__link}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
