7c9cfd6ad00ff028018378cfe13384b8ca5292c2
parkmimho <smj0441@daum.net>
2019-02-13 22:48:35 +0900

    #3-16 Moving the App Component and Adding Reset-CSS

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from "components/App/styles.module.scss";
+
+class App extends Component {
+    render() {
+        return (
+            <div className={styles.App}> hello</div>
+        )
+    }
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'index.css'
-import App from './App'
+import App from 'components/App'
 import {Provider} from 'react-redux'
 import store,{history} from './redux/configureStore'
 import { ConnectedRouter } from "connected-react-router"
