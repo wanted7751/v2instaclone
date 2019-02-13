9203ddeae0c63b2604056c5b4b06e9c4b9467403
parkmimho <smj0441@daum.net>
2019-02-13 23:25:18 +0900

    #3-18 Creating the Footer Component

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import styles from "components/App/styles.module.scss";
+import Footer from 'components/Footer'
 
 class App extends Component {
     render() {
         return (
-            <div className={styles.App}> hello</div>
+            <div className={styles.App}> 
+            <Footer />
+            </div>
         )
     }
 }
