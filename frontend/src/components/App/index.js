9bc3c7d9d8ecaef2b11c4be96202e14f39b3e2ef
parkmimho <smj0441@daum.net>
2019-02-15 12:21:11 +0900

    #3-25 Auth Component part Two

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -4,6 +4,7 @@ import Container from './container';
 
 
 const mapStateToProps = (state, ownProps) =>{
+    // store에 있는 state를 복사하는 단계
     const {user} = state;
     return {
       isLoggedIn:user.isLoggedIn
--- frontend/src/components/Auth/container.js
+++ frontend/src/components/Auth/container.js
@@ -8,21 +8,38 @@ class Container extends Component{
     }
     render(){
         const {action} = this.state;
+        console.log(this.state);
         return (
                 <Auth  action={action} changeAction={this._changeAction} />
         )
     }
+    
     _changeAction = () => {
         this.setState(prevState =>{
+            //함수형으로 하면 단순히 setState({}) 객체를 반환해야 해서 이렇게 쓰는데 함수형으로 쓰면
+            // return으로 돌려줘야 한다. 
             const {action} = prevState;
+            console.log(prevState)
             // const action = prevState.action 이와 같다. 
+            // callback??
             if(action === 'login'){
                 return{
                     action: "signup"
+                }
+            }else if(action === "signup"){
+                return {
+                    action:"login"
                 }
             }
         })
     }
 }
-
+// _changeAction = () => {
+//     const { action } = this.state;
+//     if (action === "login") {
+//         this.setState({ action: "signup" });
+//     } else {
+//         this.setState({ action: "login" });
+//     }
+// }
 export default Container;
