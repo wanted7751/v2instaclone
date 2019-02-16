a99a3d7f49e8de06087c6a6370e1b5f6d0fb6df0
parkmimho <smj0441@daum.net>
2019-02-16 22:19:45 +0900

    #3-30 Stateless Components and Context

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "components/Footer/styles.module.scss";
+import PropTypes from 'prop-types'
 
 
 
@@ -8,16 +9,16 @@ const Footer = (props, context) => (
             <div className={styles.column}>
                 <nav className={styles.nav}>
                     <ul className={styles.list}>
-                        <li className={styles.listItem}>About us</li>
-                        <li className={styles.listItem}>Support</li>
-                        <li className={styles.listItem}>Blog</li>
-                        <li className={styles.listItem}>Press</li>
-                        <li className={styles.listItem}>API</li>
-                        <li className={styles.listItem}>Jobs</li>    
-                        <li className={styles.listItem}>Privacy</li>
-                        <li className={styles.listItem}>Terms</li>
-                        <li className={styles.listItem}>Directory</li>
-                        <li className={styles.listItem}>Language</li>
+                        <li className={styles.listItem}>{context.t("About Us")}</li>
+                        <li className={styles.listItem}>{context.t("Support")}</li>
+                        <li className={styles.listItem}>{context.t("Blog")}</li>
+                        <li className={styles.listItem}>{context.t("Press")}</li>
+                        <li className={styles.listItem}>{context.t("API")}</li>
+                        <li className={styles.listItem}>{context.t("Jobs")}</li>    
+                        <li className={styles.listItem}>{context.t("Privacy")}</li>
+                        <li className={styles.listItem}>{context.t("Terms")}</li>
+                        <li className={styles.listItem}>{context.t("Directory")}</li>
+                        <li className={styles.listItem}>{context.t("Language")}</li>
                     </ul>
                 </nav>
             </div>
@@ -27,6 +28,10 @@ const Footer = (props, context) => (
             
         </footer>
 )
+
+Footer.contextTypes = {
+    t:PropTypes.func.isRequired,
+}
 
 
 export default Footer;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,10 +1,16 @@
 export const translations = {
-    "es":{
-        "Login in": "Inciar Sesion"
-    },
-    "kr":
-    {
-        "Log in" : "로그인"
+    "kr":{
+        "Login in": "로그인",
+        "About Us": "정보", 
+        "Support": "지원",
+        "Blog": "블로그",
+        "Press":"언론",
+        "API":"API",
+        "Jobs":"직업",
+        "Privacy":"정책",
+        "Terms":"기간",
+        "Directory":"안내",
+        "Language":"언어"
     }
 
 }
