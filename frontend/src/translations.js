5c0391de43035a5dd119b65a28f9974458426b3e
parkmimho <smj0441@daum.net>
2019-02-16 23:22:50 +0900

    #3-32 Importing Translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,16 +1,16 @@
 export const translations = {
-    "kr":{
-        "Login in": "로그인",
-        "About Us": "정보", 
-        "Support": "지원",
-        "Blog": "블로그",
-        "Press":"언론",
-        "API":"API",
-        "Jobs":"직업",
-        "Privacy":"정책",
-        "Terms":"기간",
-        "Directory":"안내",
-        "Language":"언어"
-    }
-
+  'kr': {
+    'About Us': '정보',
+    'Support': '지원',
+    'Blog': '블로그',
+    'Press': '기사',
+    'API': '어플리케이션 인터페이스',
+    'Jobs': '직업정보',
+    'Privacy': '정책',
+    'Terms': '기간',
+    'Directory': '사전',
+    'Language': '',
+  },
+  'options': {
+  },
 }
